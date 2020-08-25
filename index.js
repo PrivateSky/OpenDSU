202ad0115fe19b0f51eadae2e6a0df2cebe9ab92
salboaie <salboaie@gmail.com>
2020-08-25 11:40:17 +0300

    initial version

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+html API space
+*/
+
+module.exports.loadApi = function(apiSpaceName){
+    switch (apiSpaceName) {
+        case "http":return require("./http"); break;
+        case "crypto":return require("./crypto"); break;
+        case "anchoring":return require("./anchoring"); break;
+        case "bricking":return require("./bricking"); break;
+        case "bdns":return require("./bdns"); break;
+        case "dc":return require("./dc"); break;
+        case "dt":return require("./dt"); break;
+        case "keyssi":return require("./keyssi"); break;
+        case "mq":return require("./mq"); break;
+        case "notifications":return require("./notifications"); break;
+        case "resolver":return require("./resolver"); break;
+        case "sc":return require("./sc"); break;
+        default: throw new Error("Unknown API space " + apiSpaceName);
+    }
+}
---
+++ mq/index.js
@@ -0,0 +1,7 @@
+/*
+html API space
+*/
+
+module.exports.test = function(){
+    console.log("test");
+}
