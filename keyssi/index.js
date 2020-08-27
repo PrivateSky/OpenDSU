2c035fc172b96f9ebe8070a4d7abc248c7a9bdff
skutner <sava.dumitru.daniel@gmail.com>
2020-08-27 16:21:41 +0300

    added KeySSI creation functions wip

--- keyssi/index.js
+++ keyssi/index.js
@@ -1,3 +1,32 @@
-/*
-html API space
-*/
+const keySSIResolver = require("key-ssi-resolver");
+const keySSIFactory = keySSIResolver.KeySSIFactory;
+const SSITypes = keySSIResolver.SSITypes;
+
+const parse = (ssiString) => {
+    return keySSIFactory.create(ssiString);
+};
+
+const buildSeedSSI = (domain, path, specificString, control, vn, hint) => {
+    const seedSSI = keySSIFactory.create(SSITypes.SEED_SSI);
+    seedSSI.load(`${domain}/${path}`, specificString, control, vn, hint);
+    return seedSSI;
+};
+
+const buildSReadSSI = (domain, path, specificString, control, vn, hint) => {
+    const sReadSSI = keySSIFactory.create(SSITypes.SREAD_SSI);
+    sReadSSI.load(`${domain}/${path}`, specificString, control, vn, hint);
+    return sReadSSI;
+};
+
+const buildSZeroAccessSSI = (domain, path, specificString, control, vn, hint) => {
+    const sZaSSI = keySSIFactory.create(SSITypes.SZERO_ACCESS_SSI);
+    sZaSSI.load(`${domain}/${path}`, specificString, control, vn, hint);
+    return sZaSSI;
+};
+
+module.exports = {
+    parse,
+    buildSeedSSI,
+    buildSReadSSI,
+    buildSZeroAccessSSI
+};
