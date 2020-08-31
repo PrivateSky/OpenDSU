c540f471dbf67f926c7983e142b9e180ef54ced2
Paul Isache <paul@isache.com>
2020-08-31 08:21:04 +0300

    Basic bricking implementation;

--- bricking/index.js
+++ bricking/index.js
@@ -1,3 +1,32 @@
 /*
 html API space
 */
+
+const pskCrypto = require('pskcrypto');
+const edfsBrickStorage = require('edfs-brick-storage').createBrickStorageService();
+
+const getBrick = (hashLinkSSI, authToken, callback) => {
+    const hash = getHash(hashLinkSSI);
+
+
+    edfsBrickStorage.getBrick(hash, (err, brick) => {
+        if (err) {
+            return callback(err);
+        }
+
+        return callback(null, pskCrypto.privateDencrypt(authToken, brick));
+    });
+};
+
+const putBrick = (brickStorageSSI, brick, authToken, callback) => {
+    const encryptedBrick = pskCrypto.privateEncrypt(authToken, brick);
+
+    return edfsBrickStorage.putBrick(brickStorageSSI, encryptedBrick, callback);
+};
+
+const getHash = (hashLinkSSI) => {
+    // TO BE implemented
+    return hashLinkSSI
+}
+
+module.exports = { getBrick, putBrick };
