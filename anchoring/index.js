cdc4bb1a8c303403dc1f741533d2f255095a7f02
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-10-07 19:00:22 +0300

    just a todo

--- anchoring/index.js
+++ anchoring/index.js
@@ -25,7 +25,7 @@ const versions = (keySSI, authToken, callback) => {
         }
 
         const queries = anchoringServicesArray.map((service) => fetch(`${service}/anchor/versions/${keySSI.getAnchorId()}`));
-
+        //TODO: security issue (which response we trust)
         Promise.allSettled(queries).then((responses) => {
             const response = responses.find((response) => response.status === 'fulfilled');
 
