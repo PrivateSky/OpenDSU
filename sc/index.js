bee98fa8750a676d5dda877fc6733606096e69cf
skutner <sava.dumitru.daniel@gmail.com>
2020-10-19 16:53:15 +0300

    added getMainDSU function to security context API

--- sc/index.js
+++ sc/index.js
@@ -1,3 +1,11 @@
-/*
-html API space
-*/
+const getMainDSU = () => {
+    if (typeof rawDossier === "undefined") {
+        throw Error("Main DSU does not exist in the current context.");
+    }
+
+    return rawDossier;
+};
+
+module.exports = {
+    getMainDSU
+}
