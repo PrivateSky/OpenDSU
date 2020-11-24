30bd1e942254c348edd31764d1704f6df5bd1225
skutner <sava.dumitru.daniel@gmail.com>
2020-11-24 19:38:59 +0200

    added system API

---
+++ system/index.js
@@ -0,0 +1,27 @@
+const envVariables = {};
+function getEnvironmentVariable(name){
+    if (typeof envVariables[name] === "undefined") {
+        return envVariables[name];
+    }
+    return process.env[name];
+}
+function setEnvironmentVariable(name, value){
+    envVariables[name] = value;
+}
+
+function getFS(){
+    const fsName = "fs";
+    return require(fsName);
+}
+
+function getPath(){
+    const pathName = "path";
+    return require(pathName);
+}
+
+module.exports = {
+    getEnvironmentVariable,
+    setEnvironmentVariable,
+    getFS,
+    getPath
+}
