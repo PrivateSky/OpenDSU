1e6b29b2eee04ec1dfd7279f91bf47379fd2f7fb
skutner <sava.dumitru.daniel@gmail.com>
2020-11-27 12:12:17 +0200

    refactored addVersion function

--- anchoring/index.js
+++ anchoring/index.js
@@ -1,11 +1,10 @@
-const openDSU = require("opendsu");
-const bdns = openDSU.loadApi("bdns");
-const keyssi = openDSU.loadApi("keyssi");
-const {fetch, doPut} = openDSU.loadApi("http");
-const config = openDSU.loadApi("config");
+const bdns = require("../bdns");
+const keyssi = require("../keyssi");
+const crypto = require("../crypto");
+const {fetch, doPut} = require("../http");
+const config = require("../config");
 const cachedAnchoring = require("./cachedAnchoring");
 const constants = require("../moduleConstants");
-const cache = require("../cache/cachedStores").getCache(constants.CACHE.ANCHORING_CACHE);
 const promiseRunner = require("../utils/promise-runner");
 
 const isValidVaultCache = () => {
@@ -14,18 +13,18 @@ const isValidVaultCache = () => {
 
 /**
  * Get versions
- * @param {keySSI} keySSI
+ * @param {keySSI} powerfulKeySSI
  * @param {string} authToken
  * @param {function} callback
  */
-const versions = (keySSI, authToken, callback) => {
+const versions = (powerfulKeySSI, authToken, callback) => {
     if (typeof authToken === 'function') {
         callback = authToken;
         authToken = undefined;
     }
 
-    const dlDomain = keySSI.getDLDomain();
-    const anchorId = keySSI.getAnchorId();
+    const dlDomain = powerfulKeySSI.getDLDomain();
+    const anchorId = powerfulKeySSI.getAnchorId();
     if (dlDomain === constants.DOMAINS.VAULT && isValidVaultCache()) {
         return cachedAnchoring.versions(anchorId, callback);
     }
@@ -41,7 +40,7 @@ const versions = (keySSI, authToken, callback) => {
 
         //TODO: security issue (which response we trust)
         const fetchAnchor = (service) => {
-            return fetch(`${service}/anchor/versions/${keySSI.getAnchorId()}`)
+            return fetch(`${service}/anchor/versions/${powerfulKeySSI.getAnchorId()}`)
                 .then((response) => {
                     return response.json().then((hlStrings) => {
                         const hashLinks = hlStrings.map((hlString) => {
@@ -60,26 +59,21 @@ const versions = (keySSI, authToken, callback) => {
 
 /**
  * Add new version
- * @param {keySSI} keySSI
+ * @param {keySSI} powerfulKeySSI
  * @param {hashLinkSSI} newHashLinkSSI
  * @param {hashLinkSSI} lastHashLinkSSI
  * @param {string} zkpValue
  * @param {string} digitalProof
  * @param {function} callback
  */
-const addVersion = (keySSI, newHashLinkSSI, lastHashLinkSSI, zkpValue, digitalProof, callback) => {
+const addVersion = (powerfulKeySSI, newHashLinkSSI, lastHashLinkSSI, callback) => {
     if (typeof lastHashLinkSSI === "function") {
         callback = lastHashLinkSSI;
         lastHashLinkSSI = undefined;
     }
 
-    if (typeof zkpValue === "function") {
-        callback = zkpValue;
-        zkpValue = undefined;
-    }
-
-    const dlDomain = keySSI.getDLDomain();
-    const anchorId = keySSI.getAnchorId();
+    const dlDomain = powerfulKeySSI.getDLDomain();
+    const anchorId = powerfulKeySSI.getAnchorId();
     if (dlDomain === constants.DOMAINS.VAULT && isValidVaultCache()) {
         return cachedAnchoring.addVersion(anchorId, newHashLinkSSI.getIdentifier(), callback);
     }
@@ -92,37 +86,44 @@ const addVersion = (keySSI, newHashLinkSSI, lastHashLinkSSI, zkpValue, digitalPr
             return callback('No anchoring service provided');
         }
 
-        const body = {
-            hash: {
-                last: lastHashLinkSSI ? lastHashLinkSSI.getIdentifier() : null,
-                new: newHashLinkSSI.getIdentifier()
-            },
-            zkpValue,
-            digitalProof
+        const hash = {
+            last: lastHashLinkSSI ? lastHashLinkSSI.getIdentifier() : null,
+            new: newHashLinkSSI.getIdentifier()
         };
 
-        const addAnchor = (service) => {
-            return new Promise((resolve, reject) => {
-                const putResult = doPut(`${service}/anchor/add/${anchorId}`, JSON.stringify(body), (err, data) => {
-                    if (err) {
-                        return reject({
-                            statusCode: err.statusCode,
-                            message: err.statusCode === 428 ? 'Unable to add alias: versions out of sync' : err.message || 'Error'
-                        });
+        crypto.sign(powerfulKeySSI, hash.new, (err, signature) => {
+            const digitalProof = {
+                signature: crypto.encodeBase58(signature),
+                publicKey: crypto.encodeBase58(powerfulKeySSI.getPublicKey("raw"))
+            }
+
+            const body = {
+                hash,
+                digitalProof
+            };
+
+            const addAnchor = (service) => {
+                return new Promise((resolve, reject) => {
+                    const putResult = doPut(`${service}/anchor/add/${anchorId}`, JSON.stringify(body), (err, data) => {
+                        if (err) {
+                            return reject({
+                                statusCode: err.statusCode,
+                                message: err.statusCode === 428 ? 'Unable to add alias: versions out of sync' : err.message || 'Error'
+                            });
+                        }
+
+                        require("opendsu").loadApi("resolver").invalidateDSUCache(powerfulKeySSI);
+                        return resolve(data);
+                    });
+                    if(putResult) {
+                        putResult.then(resolve).catch(reject);
                     }
+                })
+            };
 
-                    require("opendsu").loadApi("resolver").invalidateDSUCache(keySSI);
-                    return resolve(data);
-                });
-                if(putResult) {
-                    putResult.then(resolve).catch(reject);
-                }
-            })
-        };
-
-        promiseRunner.runOneSuccessful(anchoringServicesArray, addAnchor, callback);
-    });
-
+            promiseRunner.runOneSuccessful(anchoringServicesArray, addAnchor, callback);
+        });
+        });
 };
 
 const getObservable = (keySSI, fromVersion, authToken, timeout) => {
