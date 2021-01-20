70907960132c1633cdd6612fc1a669c17ec45c0e
tvenceslau <tiago.venceslau@pdmfc.com>
2021-01-20 15:19:42 +0000

    adds Dossier Builder to dt Api space. SHould be run with the buildDossier octopus script

--- dt/index.js
+++ dt/index.js
@@ -1,3 +1,16 @@
 /*
 html API space
 */
+
+
+
+//const builder = new test.DossierBuilder
+const getDossierBuilder = () => {
+    return new (require("./DossierBuilder"))()
+}
+
+const test = getDossierBuilder();
+
+module.exports = {
+    getDossierBuilder
+}
