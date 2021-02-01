c83cb4ffe91a3d9944f3b104f1f0d9955b718bb4
tvenceslau <tiago.venceslau@pdmfc.com>
2021-02-01 16:47:43 +0000

    refactors DossierBuilder to accept commands other than configs

--- dt/DossierBuilder.js
+++ dt/DossierBuilder.js
@@ -1,110 +1,18 @@
-const DEFAULT_DOSSIER_SEED_FILE_PATH = "./seed";
-const DEFAULT_CARDINAL_SEED_FILE_PATH = "../cardinal/seed";
-const DEFAULT_THEMES_PATH = "../themes";
-const DEFAULT_DOMAIN = "default";
-
 const fs = require("fs");
-const path = require("path");
 const openDSU = require("opendsu");
 const keyssi = openDSU.loadApi("keyssi");
 const resolver = openDSU.loadApi("resolver");
 
-/**
- * Automates the Dossier Building process
- * Call via
- * <pre>
- *     builder.build(config, callback)
- * </pre>
- * where the config is as follows (for a theme for instance):
- * <pre>
- *     {
- *          "dossier_seed": "./seed",
- *          "domain": "default",
- *          "bundles_path": "../../privatesky/psknode/bundles",
- *          "delete": ["/"],
- *          "folders": ["src"],
- *          "copy": [],
- *          "mounts": []
- *     }
- * </pre>
- * For a Simple SSApp (with only mounting of cardinal/themes and creation of code folder):
- * <pre>
- *      {
- *          "dossier_seed": "./seed",
- *          "domain": "default",
- *          "bundles_path": "../privatesky/psknode/bundles",
- *          "delete": [
- *              "/"
- *          ],
- *          "folders": [
- *              "code"
- *          ],
- *          "copy": [],
- *          "mounts": [
- *              {
- *                  "mount_point": "/cardinal",
- *                  "type": "seed",
- *                  "arguments": {
- *                      "path": "../cardinal/seed"
- *                  }
- *               },
- *               {
- *                  "mount_point": "/themes",
- *                  "type": "folder",
- *                  "arguments": {
- *                      "path": "../themes",
- *                      "seed": "seed",
- *                      "name": "$folder"
- *                  }
- *                }
- *          ]
- *     }
- * </pre>
- *
- * each entry in the copy list should be:
- * <pre>
- *     {
- *         "from": "...",
- *         "to": "..."
- *     }
- * </pre>
- */
-const DossierBuilder = function() {
-    /**
-     * Builds the config object from a file path if necessary
-     * @param {string|object} pathOrObject: the path to the config json file or the config object
-     * @param {function} callback:
-     */
-    let load_config = function (pathOrObject, callback) {
-        if (typeof pathOrObject === 'string') {
-            fs.readFile(pathOrObject, (err, data) => {
-                if (err)
-                    return callback(err);
-                let cfg;
+const operations = {
+    DELETE: "delete",
+    ADD_FOLDER: "addfolder",
+    ADD_FILE: "addfile",
+    MOUNT: "mount"
+}
 
-                try {
-                    cfg = handle_defaults(JSON.parse(data.toString()));
-                } catch (e) {
-                    return callback(e);
-                }
+const DossierBuilder = function(){
 
-                callback(undefined, cfg);
-            });
-        } else
-            callback(undefined, handle_defaults(pathOrObject));
-    }
-    /**
-     * Fills any missing required configuration data with the defaults values
-     * @param {object} c: the provided config
-     */
-    let handle_defaults = function (c) {
-        c.dossier_seed = c.dossier_seed || DEFAULT_DOSSIER_SEED_FILE_PATH;
-        c.cardinal_seed = c.cardinal_seed || DEFAULT_CARDINAL_SEED_FILE_PATH;
-        c.themes_folder = c.themes_folder || DEFAULT_THEMES_PATH;
-        c.domain = c.domain || DEFAULT_DOMAIN;
-        return c;
-    }
-
+    let needsUpdating = false;
 
     /**
      * recursively executes the provided func with the dossier and each of the provided arguments
@@ -128,7 +36,7 @@ const DossierBuilder = function() {
                 callback(undefined, result);
             }
         });
-    }
+    };
 
     let del = function (bar, path, options, callback) {
         if (typeof options === 'function'){
@@ -137,8 +45,9 @@ const DossierBuilder = function() {
         }
         options = options || {ignoreMounts: false};
         console.log("Deleting " + path);
+        needsUpdating = true;
         bar.delete(path, options, callback);
-    }
+    };
 
     let addFolder = function (folder_root = "/") {
         return function (bar, arg, options, callback){
@@ -148,9 +57,10 @@ const DossierBuilder = function() {
             }
             options = options || {batch: false, encrypt: false};
             console.log("Adding Folder " + folder_root + arg)
+            needsUpdating = true;
             bar.addFolder(arg, folder_root, options, callback);
         };
-    }
+    };
 
     let addFile = function (bar, arg, options, callback) {
         if (typeof options === 'function'){
@@ -159,90 +69,65 @@ const DossierBuilder = function() {
         }
         options = options || {encrypt: true, ignoreMounts: false}
         console.log("Copying file " + arg.from + " to " + arg.to)
+        needsUpdating = true;
         bar.addFile(arg.from, arg.to, options, callback)
-    }
+    };
 
-    /**
-     * mounts in the provided bar according to the mount_obj
-     * @param bar
-     * @param {object} mount_obj
-     * @param {object} options
-     * @param {function} callback
-     */
-    let mount = function (bar, mount_obj, options, callback) {
+    let mount = function (bar, arg, options, callback) {
         if (typeof options === 'function'){
             callback = options;
             options = undefined
         }
 
-        if (mount_obj.type === 'folder')
-            return mount_folders(bar, mount_obj, options, callback);
-
-        readFile(mount_obj.arguments.path, (err, data) => {
+        readFile(arg.seed_path, (err, data) => {
             if (err)
                 return callback(err);
             let seed = data.toString();
-            console.log("Mounting " + mount_obj.arguments.path + " with seed " + seed + " to " + mount_obj.mount_point);
-            bar.mount(mount_obj.mount_point, seed, callback);
+            console.log("Mounting " + arg.seed_path + " with seed " + seed + " to " + arg.mount_point);
+            needsUpdating = false;
+            bar.mount(arg.mount_point, seed, callback);
         });
+    };
 
-    }
-
-    let mount_folders = function (bar, mount_obj, options, callback) {
-        let names = fs.readdirSync(mount_obj.arguments.path);
+    let mount_folders = function (bar, arg, callback) {
+        let base_path = arg.seed_path.split("*");
+        let names = fs.readdirSync(base_path[0]);
         let arguments = names.map(n => {
             return {
-                mount_point: path.join(mount_obj.mount_point, mount_obj.arguments.name.replace('$folder', n)),
-                type: "seed",
-                arguments: {
-                    path: path.join(mount_obj.arguments.path, n, "seed")
-                },
-                options: options
+                "seed_path": arg.seed_path.replace("*", n),
+                "mount_point": arg.mount_point.replace("*", n)
             };
         });
         execute(bar, mount, arguments, callback);
-    }
+    };
 
-    let createDossier = function (conf, callback) {
+    let evaluate_mount = function(bar, cmd, callback){
+        if (needsUpdating)
+            return refreshDSU(bar, (err, updatedDossier, keySSIstring) => {
+                if (err)
+                    return callback(err);
+                evaluate_mount(updatedDossier, cmd, callback)
+            });
+
+        let arguments = {
+            "seed_path": cmd[0],
+            "mount_point": cmd[1]
+        };
+
+        if (!arguments.seed_path.match(/[\\/]\*[\\/]/))
+            mount(bar, arguments, callback);             // single mount
+        else
+            mount_folders(bar, arguments, callback);     // folder mount
+    };
+
+    let createDossier = function (conf, commands, callback) {
+        console.log("creating a new dossier...")
         resolver.createDSU(keyssi.buildSeedSSI(conf.domain), (err, bar) => {
             if (err)
                 return callback(err);
-            updateDossier(bar, conf, callback);
-        })
-    }
-
-    let updateDossier = function (bar, conf, callback) {
-        execute(bar, del, conf.delete, (err, result) => {
-            if (err)
-                return callback(err);
-
-            execute(bar, addFolder('/'), conf.folders, (err, result) => {
-                if (err)
-                    return callback(err);
-
-                execute(bar, addFile, conf.copy, (err, result) => {
-                    if (err)
-                        return callback(err);
-
-                    bar.getKeySSIAsString((err, barKeySSI) => {
-                        if (err)
-                            return callback(err);
-
-                        resolver.loadDSU(barKeySSI, (err, loadedDossier) => {
-                            if (err)
-                                return callback(err);
-
-                            execute(loadedDossier, mount, conf.mounts, (err, result) => {
-                                if (err)
-                                    return callback(err);
-                                storeKeySSI(conf.dossier_seed, barKeySSI, callback);
-                            });
-                        });
-                    });
-                });
-            });
+            updateDossier(bar, conf, commands, callback);
         });
-    }
+    };
 
     let readFile = function (filePath, callback) {
         fs.readFile(filePath, (err, content) => {
@@ -251,7 +136,7 @@ const DossierBuilder = function() {
 
             callback(undefined, content.toString());
         })
-    }
+    };
 
     let writeFile = function (filePath, data, callback) {
         fs.writeFile(filePath, data, (err) => {
@@ -259,46 +144,102 @@ const DossierBuilder = function() {
                 return callback(err);
             callback(undefined, data.toString());
         });
-    }
+    };
 
     let storeKeySSI = function (seed_path, keySSI, callback) {
         writeFile(seed_path, keySSI, callback);
-    }
+    };
 
-    this.build = function (config, callback) {
-        load_config(config, (err, conf) => {
+    let runCommand = function(bar, command, cfg, callback){
+        let cmd = command.split(/\s+/);
+        switch (cmd.shift().toLowerCase()){
+            case operations.DELETE:
+                execute(bar, del, cmd, callback);               // allows for multi argument delete
+                //del(bar, cmd[0], callback);
+                break;
+            case operations.ADD_FOLDER:
+                execute(bar, addFolder(), cmd, callback);       // allows for multi argument add folder
+                //addFolder()(bar, cmd[0], callback);
+                break;
+            case operations.ADD_FILE:
+                let arg = {
+                    "from": cmd[0],
+                    "to": cmd[1]
+                }
+                addFile(bar, arg, callback);
+                break;
+            case operations.MOUNT:
+                evaluate_mount(bar, cmd, callback)
+                break;
+            default:
+                return callback(new Error("Invalid operation requested: " + command));
+        }
+    };
+
+    let refreshDSU = function(bar, callback){
+        bar.getKeySSIAsString((err, barKeySSI) => {
             if (err)
                 return callback(err);
 
-            readFile(conf.dossier_seed, (err, content) => {
-                if (err || content.length === 0) {
-                    console.log(`Creating a new Dossier...`);
-                    return createDossier(conf, callback);
-                }
-
-                let keySSI;
-                try {
-                    keySSI = keyssi.parse(content.toString());
-                } catch (err) {
-                    console.log("Invalid keySSI. Creating a new Dossier...");
-                    return createDossier(conf, callback);
-                }
-
-                if (keySSI.getDLDomain() !== conf.domain) {
-                    console.log("Domain change detected. Creating a new Dossier...");
-                    return createDossier(conf, callback);
-                }
-
-                console.log("Dossier updating...");
-                resolver.loadDSU(content.toString(), (err, bar) => {
-                    if (err)
-                        return callback(err);
-
-                    updateDossier(bar, conf, callback);
-                });
+            resolver.loadDSU(barKeySSI, (err, loadedDossier) => {
+                if (err)
+                    return callback(err);
+                needsUpdating = false;
+                callback(undefined, loadedDossier, barKeySSI);
             });
         });
-    }
-}
+    };
+
+    let saveDSU = function(bar, cfg, callback){
+        refreshDSU(bar, (err, loadedDossier, barKeySSI) => {
+            if (err)
+                return callback(err);
+            storeKeySSI(cfg.seed, barKeySSI, callback);
+        });
+    };
+
+    let updateDossier = function(bar, cfg, commands, callback) {
+        if (commands.length === 0)
+            return saveDSU(bar, cfg, callback);
+        let cmd = commands.shift();
+        runCommand(bar, cmd, cfg, (err, result) => {
+            if (err)
+                return callback(err);
+            updateDossier(bar, cfg, commands, callback);
+        });
+    };
+
+    this.buildDossier = function(cfg, commands, callback){
+        if (typeof commands === 'function'){
+            callback = commands;
+            commands = [];
+        }
+
+        readFile(cfg.seed, (err, content) => {
+            if (err || content.length === 0)
+                return createDossier(cfg, commands, callback);
+
+            let keySSI;
+            try {
+                keySSI = keyssi.parse(content.toString());
+            } catch (err) {
+                console.log("Invalid keySSI");
+                return createDossier(cfg, commands, callback);
+            }
+
+            if (keySSI.getDLDomain() !== cfg.domain) {
+                console.log("Domain change detected.");
+                return createDossier(cfg, commands, callback);
+            }
+
+            console.log("Dossier updating...");
+            resolver.loadDSU(content.toString(), (err, bar) => {
+                if (err)
+                    return callback(err);
+                updateDossier(bar, cfg, commands, callback);
+            });
+        });
+    };
+};
 
 module.exports = DossierBuilder;
