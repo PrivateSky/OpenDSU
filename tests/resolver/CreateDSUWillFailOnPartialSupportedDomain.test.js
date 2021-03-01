18ad594ea398b547c382e0345e84877b426c9242
skutner <sava.dumitru.daniel@gmail.com>
2021-03-01 18:58:21 +0200

    fixed tests

--- tests/resolver/CreateDSUWillFailOnPartialSupportedDomain.test.js
+++ tests/resolver/CreateDSUWillFailOnPartialSupportedDomain.test.js
@@ -10,38 +10,31 @@ const keySSI = require("../../keyssi")
 
 assert.callback('Create DSU on partial supported domain will fail', (testfinished) => {
 
-    dc.createTestFolder('createDSU',(err,folder) => {
+    dc.createTestFolder('createDSU', (err, folder) => {
         testIntegration.launchApiHubTestNode(10, folder, (err) => {
             const domain = 'testdomain';
             prepareBDNSContext(folder);
-            createDSU(domain ,(err, dus) => {
+            createDSU(domain, (err, dus) => {
 
                 printOpenDSUError(err);
-                assert.notEqual(typeof err,'undefined');
+                assert.notEqual(typeof err, 'undefined');
 
                 testfinished();
-
             });
         })
-        })
-
-
-
-
+    })
 }, 5000);
 
 
-function createDSU(domain, callback)
-{
-    const keyssitemplate = keySSI.buildTemplateKeySSI('seed',domain);
+function createDSU(domain, callback) {
+    const keyssitemplate = keySSI.createTemplateKeySSI('seed', domain);
     resolver.createDSU(keyssitemplate, {bricksDomain: domain}, (err, dsu) => {
         callback(err, dsu);
     });
 }
 
 
-function prepareBDNSContext(folder)
-{
+function prepareBDNSContext(folder) {
 
     let bdns = {
         'testdomain': {
@@ -55,6 +48,6 @@ function prepareBDNSContext(folder)
         }
     }
 
-    require('fs').writeFileSync(folder+'/external-volume/config/bdns.hosts', JSON.stringify(bdns));
+    require('fs').writeFileSync(folder + '/external-volume/config/bdns.hosts', JSON.stringify(bdns));
 
 }
