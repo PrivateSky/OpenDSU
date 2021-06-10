86201f36e70d23569f175b41c7bdeac5542ccd7f
skutner <sava.dumitru.daniel@gmail.com>
2021-06-10 13:12:35 +0300

    tested resolving key did

--- tests/w3cdid/key_DID_SSI_test.js
+++ tests/w3cdid/key_DID_SSI_test.js
@@ -3,7 +3,8 @@ const tir = require("../../../../psknode/tests/util/tir");
 
 const dc = require("double-check");
 const assert = dc.assert;
-const openDSU = require('opendsu');
+const openDSU = require('../../index');
+$$.__registerModule("opendsu", openDSU);
 const keySSI = openDSU.loadAPI("keyssi");
 const resolver = openDSU.loadAPI("resolver");
 const scAPI = openDSU.loadAPI("sc");
@@ -14,41 +15,34 @@ assert.callback('key DID SSI test', (testFinished) => {
     let sc;
 
     dc.createTestFolder('createDSU', (err, folder) => {
-        tir.launchApiHubTestNode(10, folder, (err) => {
+        tir.launchApiHubTestNode(10, folder, async (err) => {
             if (err) {
                 throw err;
             }
-            const initializeSC = (callback) => {
-                resolver.createSeedDSU(domain, (err, seedDSU) => {
-                    if (err) {
-                        return callback(err);
-                    }
-                    seedDSU.getKeySSIAsObject((err, seedSSI) => {
-                        if (err) {
-                            return callback(err);
-                        }
-                        sc = scAPI.getSecurityContext(seedSSI);
-                        callback()
-                    });
-                })
-            }
-            initializeSC(async (err) => {
-                if (err) {
-                    throw err;
-                }
+            const initializeSC = async () => {
                 try {
-                    const seedSSI = await $$.promisify(keySSI.createSeedSSI)(domain);
-                    const didDocument = await $$.promisify(w3cDID.createIdentity)("key", seedSSI);
-
-                    const dataToSign = "someData";
-                    const signature = await $$.promisify(didDocument.sign)(dataToSign);
-                    const verificationResult = await $$.promisify(didDocument.verify)(dataToSign, signature);
-                    assert.true(verificationResult, "Failed to verify signature");
-                    testFinished();
+                    const seedDSU = await $$.promisify(resolver.createSeedDSU)(domain);
+                    const seedSSI = await $$.promisify(seedDSU.getKeySSIAsObject)()
+                    sc = scAPI.getSecurityContext(seedSSI);
                 } catch (e) {
                     throw e;
                 }
-            });
+            }
+
+            try {
+                await initializeSC();
+                const seedSSI = await $$.promisify(keySSI.createSeedSSI)(domain);
+                const didDocument = await $$.promisify(w3cDID.createIdentity)("key", seedSSI);
+
+                const dataToSign = "someData";
+                const signature = await $$.promisify(didDocument.sign)(dataToSign);
+                const resolvedDIDDocument = await $$.promisify(w3cDID.resolveDID)(didDocument.getIdentifier());
+                const verificationResult = await $$.promisify(resolvedDIDDocument.verify)(dataToSign, signature);
+                assert.true(verificationResult, "Failed to verify signature");
+                testFinished();
+            } catch (e) {
+                throw e;
+            }
         });
     });
 }, 5000);
