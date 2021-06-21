401d3aa84792f66734f4154f8a568c48fe995646
Daniel Visoiu <9691701+daniel-visoiu@users.noreply.github.com>
2021-06-21 23:39:41 +0300

    Fix contracts test utils due to bricksledger changes.

--- tests/contracts/utils.js
+++ tests/contracts/utils.js
@@ -10,22 +10,20 @@ async function launchApiHubTestNodeWithTestDomain(options, callback) {
         callback = options;
         options = {
             setConstitutionInConfig: true,
-            setDefaultContractsDomainInEnv: false,
             keepOnlyContractsConfigInsideDomainConfig: false,
         };
     }
 
-    const {
-        setConstitutionInConfig = true,
-        setDefaultContractsDomainInEnv = false,
-        keepOnlyContractsConfigInsideDomainConfig = false,
-    } = options;
+    const { setConstitutionInConfig = true, keepOnlyContractsConfigInsideDomainConfig = false } = options;
 
     try {
         const folder = await $$.promisify(dc.createTestFolder)("dsu");
-        const domainsConfigPath = path.join(folder, "/external-volume/config/domains");
+        const configPath = path.join(folder, "/external-volume/config");
+        const domainsConfigPath = path.join(configPath, "domains");
 
         let serverConfig;
+        let testDomainConfig;
+
         if (keepOnlyContractsConfigInsideDomainConfig) {
             // in order to keep only contracts config inside domain config file, we need to put inside serverconfig the non-contracts configs
             serverConfig = {
@@ -52,6 +50,10 @@ async function launchApiHubTestNodeWithTestDomain(options, callback) {
                         domainsPath: "/external-volume/domains",
                     },
                 },
+            };
+
+            testDomainConfig = {
+                contracts: {},
             };
         } else {
             serverConfig = {};
@@ -80,9 +82,25 @@ async function launchApiHubTestNodeWithTestDomain(options, callback) {
             };
 
             await $$.promisify(testIntegration.storeFile)(domainsConfigPath, "default.json", JSON.stringify(defaultDomainConfig));
+
+            testDomainConfig = {
+                anchoring: {
+                    type: "Contract",
+                    option: {
+                        path: "/external-volume/domains/contract/anchors",
+                        enableBricksLedger: false,
+                    },
+                },
+                bricking: {
+                    path: "/external-volume/domains/contract/brick-storage",
+                },
+            };
         }
 
         await $$.promisify(testIntegration.storeServerConfig)(folder, serverConfig);
+
+        // store domain config
+        await $$.promisify(testIntegration.storeFile)(domainsConfigPath, "contract.json", JSON.stringify(testDomainConfig));
 
         await $$.promisify(testIntegration.launchApiHubTestNode)(10, folder);
         await $$.promisify(testIntegration.addDomainsInBDNS.bind(testIntegration))(folder, ["contract"]);
@@ -106,38 +124,17 @@ async function launchApiHubTestNodeWithTestDomain(options, callback) {
         const domainSeed = fs.readFileSync(domainSeedPath, { encoding: "utf8" });
         console.log("domainSeed", domainSeed);
 
-        // store domain config
-        let testDomainConfig;
-        if (keepOnlyContractsConfigInsideDomainConfig) {
-            testDomainConfig = {
-                contracts: {
-                    constitution: setConstitutionInConfig ? domainSeed : "",
-                },
-            };
-        } else {
-            testDomainConfig = {
-                anchoring: {
-                    type: "Contract",
-                    option: {
-                        path: "/external-volume/domains/contract/anchors",
-                        enableBricksLedger: false,
-                    },
-                },
-                bricking: {
-                    path: "/external-volume/domains/contract/brick-storage",
-                },
+        process.env.PSK_APIHUB_DEFAULT_CONTRACTS_DOMAIN_SSI = domainSeed;
 
-                contracts: {
-                    constitution: setConstitutionInConfig ? domainSeed : "",
-                },
-            };
-        }
+        const constants = require("opendsu").constants;
+        const w3cDID = require("opendsu").loadApi("w3cdid");
+        const validatorDID = await $$.promisify(w3cDID.createIdentity)("demo", "id");
 
-        await $$.promisify(testIntegration.storeFile)(domainsConfigPath, "contract.json", JSON.stringify(testDomainConfig));
-
-        if (setDefaultContractsDomainInEnv) {
-            process.env.PSK_APIHUB_DEFAULT_CONTRACTS_DOMAIN_SSI = domainSeed;
-        }
+        const contractBdnsConfig = {
+            validators: [{ DID: validatorDID.getIdentifier(), URL: process.env[constants.BDNS_ROOT_HOSTS] }],
+        };
+        const bdnsConfigPath = path.join(configPath, "bdns");
+        await $$.promisify(testIntegration.storeFile)(bdnsConfigPath, "contract.json", JSON.stringify(contractBdnsConfig));
 
         callback();
     } catch (error) {
