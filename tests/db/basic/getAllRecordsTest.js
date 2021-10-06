3b99ab6733c4d2674587aeeae3a59f3cf8722864
skutner <sava.dumitru.daniel@gmail.com>
2021-10-06 17:54:49 +0300

    fixed test

--- tests/db/basic/getAllRecordsTest.js
+++ tests/db/basic/getAllRecordsTest.js
@@ -22,13 +22,25 @@ assert.callback('Get all records test', (testFinished) => {
         try {
             await tir.launchConfigurableApiHubTestNodeAsync({domains: [{name: "vault", config: vaultDomainConfig}]});
             const TABLE = "test_table";
-            let records = [{pk:"key1", record:{"value_1": 1}}, {pk:"key2", record:{"value_2": 2}}, {pk:"key3", record:{"value_3": 3}}, {pk:"key4", record:{"value_4": 5}}];
+            let records = [{pk:"key1", record:{"value": 1}}, {pk:"key2", record:{"value": 2}}, {pk:"key3", record:{"value": 3}}, {pk:"key4", record:{"value": 5}}];
             const db = dbAPI.getSimpleWalletDB("mydb");
             for (let i = 0; i < records.length; i++) {
                 await $$.promisify(db.insertRecord)(TABLE, records[i].pk, records[i].record);
             }
             const tableContent = await $$.promisify(db.getAllRecords)(TABLE);
             records = records.map(e => e.record);
+            tableContent.sort((a, b)=>{
+                if (a.value < b.value) {
+                    return -1;
+                }
+
+                if (a.value === b.value) {
+                    return 0
+                }
+
+                return 1;
+            })
+
             assert.arraysMatch(tableContent, records);
             testFinished();
         } catch (e) {
