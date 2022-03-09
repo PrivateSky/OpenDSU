c8773697a96c24ca0514a2e603ba4186b30aee68
skutner <sava.dumitru.daniel@gmail.com>
2022-03-09 10:58:40 +0200

    return an error with statusCode 428 when signature verification fails

--- anchoring/anchoringAbstractBehaviour.js
+++ anchoring/anchoringAbstractBehaviour.js
@@ -68,13 +68,10 @@ function AnchoringAbstractBehaviour(persistenceStrategy) {
             const lastSignedHashLinkKeySSI = keySSISpace.parse(data[data.length - 1]);
             const dataToVerify = anchorValueSSIKeySSI.getDataToSign(anchorIdKeySSI, lastSignedHashLinkKeySSI);
             if (!signer.verify(dataToVerify, signature)) {
-                return callback(Error("Failed to verify the signature!"));
+                return callback({statusCode: 428, message: "Versions out of sync"});
             }
-            persistenceStrategy.appendAnchor(anchorIdKeySSI.getAnchorId(), anchorValueSSIKeySSI.getIdentifier(), (err) => {
-                return callback(err);
-            });
+            persistenceStrategy.appendAnchor(anchorIdKeySSI.getAnchorId(), anchorValueSSIKeySSI.getIdentifier(), callback);
         })
-
     }
 
     self.getAllVersions = function (anchorId, callback) {
@@ -160,14 +157,12 @@ function AnchoringAbstractBehaviour(persistenceStrategy) {
             let hashLinkSSI = historyOfKeySSIValues[i];
             if (hashLinkSSI.isTransfer()) {
                 return {
-                    wasTransferred: true,
-                    signVerifier: hashLinkSSI
+                    wasTransferred: true, signVerifier: hashLinkSSI
                 };
             }
         }
         return {
-            wasTransferred: false,
-            signVerifier: undefined
+            wasTransferred: false, signVerifier: undefined
         }
     }
 }
