c56fcc5605d184a84f839b276e52a7e6ffc0631c
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2022-04-12 14:31:44 +0300

    refactored addfolder command from dt to recognize the destination path

--- dt/commands/addFolder.js
+++ dt/commands/addFolder.js
@@ -35,7 +35,7 @@ class AddFolderCommand extends Command {
             callback = next;
             next = undefined;
         }
-        callback(undefined, command[0]);
+        callback(undefined, command);
     }
 
     /**
@@ -56,9 +56,13 @@ class AddFolderCommand extends Command {
         }
 
         options = options || {batch: false, encrypt: false};
-        console.log("Adding Folder " + '/' + arg)
-        bar.addFolder(arg, '/', options, err => err
-            ? _err(`Could not add folder at '${'/' + arg}'`, err, callback)
+        let src = arg[0];
+        let dst = arg[1] || "/";
+        let commandLog = "Adding Folder " + src + " to " + dst;
+        console.log(commandLog);
+
+        bar.addFolder(src, dst, options, err => err
+            ? _err(`Failed to: `+commandLog, err, callback)
             : callback(undefined, bar));
     }
 }
