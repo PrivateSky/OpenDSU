8f0cc7badd42eeb55b8bed85464c3dfc52924f19
skutner <sava.dumitru.daniel@gmail.com>
2022-07-11 17:43:50 +0300

    changed statusCode property to status on node fetch

--- http/node/fetch.js
+++ http/node/fetch.js
@@ -139,7 +139,7 @@ function Response(httpRequest, httpResponse) {
 	}
 
 	this.ok = httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 ? true : false;
-	this.statusCode = httpResponse.statusCode;
+	this.status = httpResponse.statusCode;
 	this.statusMessage = httpResponse.statusMessage;
 	this.headers = httpResponse.headers;
 
